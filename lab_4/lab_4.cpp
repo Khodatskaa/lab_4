8d7e2ca836397f381c5f963616646875f4fe56d1
Khodatskaa <khodatskaalina@gmail.com>
2025-05-24 14:19:54 +0300

    +

--- lab_4/lab_4.cpp
+++ lab_4/lab_4.cpp
@@ -1,11 +1,69 @@
 ﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
 using namespace std;
 
-// 
+// знайти дві різні вершини в кореневому дереві, що мають один колір і жодна з них не є предком іншої
 
-int main()
-{
+const int MAXN = 2e5 + 5;
 
+vector<int> tree[MAXN];
+int tin[MAXN], tout[MAXN], timer = 0;
+int color[MAXN];
 
-	return 0;
+void dfs(int v) {
+    tin[v] = ++timer;
+    for (int u : tree[v]) {
+        dfs(u);
+    }
+    tout[v] = ++timer;
+}
+
+bool isAncestor(int u, int v) {
+    return tin[u] <= tin[v] && tout[v] <= tout[u];
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    for (int i = 2; i <= n; ++i) {
+        int p;
+        cin >> p;
+        tree[p].push_back(i);
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        cin >> color[i];
+    }
+
+    dfs(1);
+
+    unordered_map<int, vector<int>> groups;
+    for (int i = 1; i <= n; ++i) {
+        groups[color[i]].push_back(i);
+    }
+
+    for (auto& [col, nodes] : groups) {
+        if (nodes.size() < 2) continue;
+
+        sort(nodes.begin(), nodes.end(), [](int a, int b) {
+            return tin[a] < tin[b];
+            });
+
+        for (int i = 0; i + 1 < (int)nodes.size(); ++i) {
+            int u = nodes[i], v = nodes[i + 1];
+            if (!isAncestor(u, v) && !isAncestor(v, u)) {
+                cout << "yes\n" << u << " " << v << "\n";
+                return 0;
+            }
+        }
+    }
+
+    cout << "no\n";
+    return 0;
 }
